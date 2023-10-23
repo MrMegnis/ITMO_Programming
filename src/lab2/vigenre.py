8f3719bb34bc25ca66ff57fe83af89446d0e706c
MrMegnis <mr.megnis@mail.ru>
2023-10-23 12:42:47 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -9,7 +9,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    symbols_amount = ord('z') - ord('a') + 1
+    text_index = 0
+    key_index = 0
+    keyword = keyword.upper()
+    while text_index < len(plaintext):
+        current_shift = ord(keyword[key_index]) - ord("A")
+        current_symbol = plaintext[text_index]
+        if current_symbol.isalpha():
+            if current_symbol.isupper():
+                ciphertext += chr(ord("A") + (ord(current_symbol) - ord("A") + current_shift) % symbols_amount)
+            else:
+                ciphertext += chr(ord("a") + (ord(current_symbol) - ord("a") + current_shift) % symbols_amount)
+        else:
+            ciphertext += current_symbol
+        text_index += 1
+        key_index = (key_index + 1) % len(keyword)
     return ciphertext
 
 
@@ -24,5 +39,11 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
+    symbols_amount = ord('z') - ord('a') + 1
+    new_key = "".join([chr(2 * ord("A") + symbols_amount - ord(i)) for i in keyword.upper()])
+    plaintext = encrypt_vigenere(ciphertext, new_key)
     # PUT YOUR CODE HERE
     return plaintext
+
+if __name__ == "__main__":
+    print(decrypt_vigenere("PYTHON", "A"))
